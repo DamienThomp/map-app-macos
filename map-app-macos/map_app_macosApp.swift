8eaf55823fa37e0cbc6031f65e23359ef717640e
Damien Thompson <sedativo@gmail.com>
2024-07-22 11:54:53 -0400

    Initial Commit

---
+++ map-app-macos/map_app_macosApp.swift
@@ -0,0 +1,17 @@
+//
+//  map_app_macosApp.swift
+//  map-app-macos
+//
+//  Created by Damien L Thompson on 2024-07-22.
+//
+
+import SwiftUI
+
+@main
+struct map_app_macosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
